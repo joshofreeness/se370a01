28dcb919469151aaf34cdfbd67d2b5eb5d45d676
joshofreeness <joshofreeness@gmail.com>
2014-07-30 20:08:27 +1200

    Added a pwd implimentation and it will continually loop like a shell

--- jpy.py
+++ jpy.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """jpy.py: A python shell. Written by Joshua Free (jfre553 2646577)"""
 
 
 import os
+
+while (True):
+	arg = input(os.getcwd() + "$> ")
+
+	if(arg == "pwd"):
+		print(os.getcwd())
+		continue
+
+
+
+
 
 
